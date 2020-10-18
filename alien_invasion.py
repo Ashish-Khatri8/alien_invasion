cd51eed7d2db54ea3045f8dc41ed8c87c15e9ab9
Ashish Khatri <khatri8.ashish@gmail.com>
2020-10-18 15:38:41 +0530

    Gave the ship ability to move left and right.

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,8 +28,9 @@ class AlienInvasion:
         Start the main loop for the game.
         """
         while True:
-            self._check_events()  # Helper method.
-            self._update_screen() # Helper method.
+            self._check_events()  # Checks for events.
+            self.ship.update()    # Updates ship's position before updating the screen.
+            self._update_screen() # Updates the screen.
 
     
     def _check_events(self):
@@ -39,6 +40,16 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True    
+            elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False                    
 
 
     def _update_screen(self):
