65d5732fc8572e76c04a26f1ca0802a7fee395bd
Ashish Khatri <khatri8.ashish@gmail.com>
2020-10-21 22:55:04 +0530

    Added movement to alien fleet.

--- alien_invasion.py
+++ alien_invasion.py
@@ -46,6 +46,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     
@@ -104,6 +105,14 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
     
+    def _update_aliens(self):
+        """
+        Check if the fleet is at the egde,
+            then update the positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+    
     def _create_fleet(self):
         """
         Create the fleet of aliens.
@@ -118,8 +127,8 @@ class AlienInvasion:
         #  Determine the number of rows of aliens that fit on the screen.
         ship_height = self.ship.rect.height
         available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
-        number_rows = available_space_y // (2 * alien_height
-        )
+        number_rows = available_space_y // (2 * alien_height)
+
         # Create the full fleet of aliens.
         for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
@@ -135,6 +144,24 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + (2 * alien.rect.height * row_number)
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """
+        Respond appropriately if any aliens have reached an edge.
+        """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+
+    def _change_fleet_direction(self):
+        """
+        Drop the entire fleet and change the fleet's direction.
+        """
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1            
     
     def _update_screen(self):
         """
